49768030796cd935719de8b5150930267ef78a01
agyanim <agyanimconsultancy@gmail.com>
2023-02-17 16:00:29 +0000

    post api added

--- express.js
+++ express.js
@@ -1,21 +1,53 @@
 const Express = require("express");
+const Cors =require("cors")
 const App = Express();
-const product = require("./products");
+const {products,emailNotification} = require("./products");
+
+// App.use(Express.urlencoded({extended:false}))
+App.use(Express.json())
+App.use(Cors())
 
 App.get("/", (req, res) => {
   res.send('<h1>BBS API.</h1><a href="/api/products"> product </a>');
 });
 App.get("/api/products", (req, res) => {
-  const newProduct = product.map((product) => {
+  const newProduct = products.map((product) => {
     const { id, name } = product;
     return { id, name };
   });
   res.status(200).json(newProduct);
 });
 
+App.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (singleProduct) {
+    return res.status(200).json(singleProduct);
+  } else return res.send("Item not fund");
+});
+
+App.get("/api/v1/query",(req,res)=>{
+  let {search}=req.query
+  const products = [...products];
+  const sortedItem=products.filter(product=>product.name.startsWith(search))
+  res.send(sortedItem)
+  // console.log(req.query.name)
+})
+
+ App.post("/api/email/notification",(req,res)=>{
+  console.log("Connection successful");
+  const data=req.body
+  emailNotification.push(data)
+  res.status(200).json({Success:true, data:emailNotification})
+ })
+
+
 App.all("*", (req, res) => {
   res.send("Sorry, The page is not available");
 });
+
 App.listen(8080, () => {
   console.log("Server started and listening on port 8080..");
 });
--- products.js
+++ products.js
@@ -1,8 +1,26 @@
-products = [
-  { id: "1", name: "Pencil", qty: 5, description: "for pupils below 5 years" },
-  { id: "2", name: "Pen", qty: 50, description: "for primary and seconday students" },
-  { id: "3", name: "Erazer", qty: 35, description: "for preschool students only" },
-  { id: "1", name: "Note 1", qty: 40, description: "for all staff"}
-]
+const products = [
+  { id: 1, name: "Pencil", qty: 5, description: "for pupils below 5 years" },
+  {
+    id: 2,
+    name: "Pen",
+    qty: 50,
+    description: "for primary and seconday students",
+  },
+  {
+    id: 3,
+    name: "Erazer",
+    qty: 35,
+    description: "for preschool students only",
+  },
+  { id: 4, name: "Note 1", qty: 40, description: "for all staff" },
+];
 
-module.exports=products
+
+const people = [{ name: "Gideon", age: 24, phone: "0245798608" }];
+const emailNotification=[
+  {email:"example@gmail.com",Date:"12/5/2023"}
+];
+
+
+
+module.exports ={products,people,emailNotification};
