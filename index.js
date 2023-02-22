8b65a52db4bf98db0e2cc9684e5b91a6fe8c2a05
agyanim <agyanimconsultancy@gmail.com>
2023-02-22 13:09:03 +0000

    merged

--- index.js
+++ index.js
@@ -3,23 +3,9 @@ const app = express();
 const cors = require("cors");
 const emailNotification = require("./routes/emailNotification");
 const others = require("./routes/other");
-<<<<<<< HEAD
-const dbConnect = require("./connections/connectDB");
-const mongoose = require("mongoose");
-const { model, Schema } = mongoose;
-
-const studentSchema = new Schema({
-  fName: String,
-  lName: String,
-  email: String,
-});
-
-const Student = model("Student", studentSchema);
-=======
 const dbConnect = require("./connections/dbConnect");
 const mongoose = require("mongoose");
 const {insertIntoCounselorInfo,display}=require("./controllers/counselors/counselorInfo.js")
->>>>>>> e7776154e40be92689b5f6eaefc1af4df696fe20
 //this is used to handle json data from the front end
 app.use(express.json());
 // this handles cross platform of information due to different server for the project
@@ -31,8 +17,8 @@ app.use(others);
 
 dbConnect();
 mongoose.connection.once("open", () => {
-  // insertIntoCounselorInfo()
-display()
+  insertIntoCounselorInfo()
+// display()
   app.listen(5000, () => {
     console.log("Server started and listening on port 500..");
   });
